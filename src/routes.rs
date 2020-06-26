f967049d45e278da17f07cca2b8309e59cccff7e
ShaneEverittM <shaneemurphy146@gmail.com>
2020-06-25 23:01:37 -0700

    Comments.

--- src/routes.rs
+++ src/routes.rs
@@ -1,25 +1,28 @@
 use diesel::{self, insert_into, prelude::*};
 
 use rocket::response::status::{Conflict, NotFound};
-use rocket_contrib::json::Json; //Easy Json coercion
+use rocket_contrib::json::Json; // Easy Json coercion
+                                // use diesel::debug_query;
 
-use crate::models::*; //Models needed for pulling or pushing data
+use crate::models::*; // Models needed for pulling or pushing data
 use crate::DbConn; // The state managed DB connection
 
-// use diesel::debug_query;
+// Aggregate struct to represent an entire quiz coming out of the db.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FullQuiz {
     quiz: Quiz,
     questions: Vec<Question>,
     answers: Vec<Vec<Answer>>,
 }
+
+//Aggregate struct to represent an entire incoming quiz to be processed before going into the db.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NewFullQuiz {
     quiz: NewQuiz,
     questions: Vec<IncomingQuestion>,
     answers: Vec<Vec<IncomingAnswer>>,
 }
-
+// Test route.
 #[get("/")]
 pub fn index(conn_ptr: DbConn) -> Result<Json<Vec<Quiz>>, String> {
     use crate::schema::quiz::dsl::*; //convenience re-exports from 'table!' macro codegen
@@ -29,7 +32,8 @@ pub fn index(conn_ptr: DbConn) -> Result<Json<Vec<Quiz>>, String> {
         Err(msg) => Err(format!("Error loading quiz: {}", msg).into()),
     }
 }
-
+// This route handles retrieval of all of the constituant parts of a quiz from their
+// tables and assembles them into a large struct and sends it as JSON.
 #[get("/quiz/<target_id>")]
 pub fn get_quiz(target_id: i32, conn_ptr: DbConn) -> Result<Json<FullQuiz>, NotFound<String>> {
     use crate::schema::answer::dsl::{answer, q_id};
@@ -65,7 +69,9 @@ no_arg_sql_function!(
     last_insert_id,
     diesel::sql_types::Unsigned<diesel::sql_types::BigInt>
 );
-
+// This route handles adding new quizzes to the db. Takes a large amount of data in the body
+// and deserializes it as well as destructuring it into its fields for insertion into their
+// respective tables.
 #[post("/quiz", format = "json", data = "<f_quiz>")]
 pub fn insert_quiz(
     f_quiz: Json<NewFullQuiz>,
@@ -83,7 +89,6 @@ pub fn insert_quiz(
         .values(q)
         .execute(conn)
         .map_err(|msg| Conflict(Some(msg.to_string())))?;
-
     let last_qz_id: u64 = diesel::select(last_insert_id)
         .first(conn)
         .map_err(|msg| Conflict(Some(msg.to_string())))?;
